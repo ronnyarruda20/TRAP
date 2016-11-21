9f3feb63b25b85a41d9869c5fff37fbac9f6a179
ronnyarruda20 <ronnyapinho@gmail.com>
2016-11-21 14:45:18 -0300

    preparado para deploy no heruk

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/app'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
